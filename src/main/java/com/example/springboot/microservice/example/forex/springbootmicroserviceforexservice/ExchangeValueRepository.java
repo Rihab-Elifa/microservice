b5c00812e198676df3408df01ed0e859176b1182
Rihab <elifarihab@gmail.com>
2024-02-19 22:35:07 +0100

    1 microservice

---
+++ src/main/java/com/example/springboot/microservice/example/forex/springbootmicroserviceforexservice/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboot.microservice.example.forex.springbootmicroserviceforexservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ExchangeValueRepository extends
+        JpaRepository<ExchangeValue, Long>{
+    ExchangeValue findByFromAndTo(String from, String to);
+}
